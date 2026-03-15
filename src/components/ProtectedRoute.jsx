c40c831d13d0a4ee365fdb5f38ce2acf49b622ed
maheshxdev <darjimaheshh05@gmail.com>
2026-03-15 16:14:59 +0530

    final

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,9 @@
-import { Navigate } from "react-router-dom";
-import { useUser } from "../context/UserData";
-
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useUser();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading || user === undefined) {
+    return <div>Loading...</div>;
+  }
 
   return user ? children : <Navigate to="/login" />;
 };
-
-export default ProtectedRoute;
