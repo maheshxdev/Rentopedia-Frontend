227e62f2bf24233aa6949384fef655e958e576c7
maheshxdev <darjimaheshh05@gmail.com>
2026-03-15 16:10:14 +0530

    final

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../context/UserData";
 
@@ -6,6 +5,7 @@ const ProtectedRoute = ({ children }) => {
   const { user, loading } = useUser();
 
   if (loading) return <div>Loading...</div>;
+
   return user ? children : <Navigate to="/login" />;
 };
 
--- src/context/UserData.jsx
+++ src/context/UserData.jsx
@@ -1,22 +1,38 @@
-// context/UserData.js
 import { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
-axios.defaults.withCredentials = true; // send httpOnly cookie
+axios.defaults.withCredentials = true;
 
 const UserContext = createContext();
 
 const UserData = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const res = await axios.get(
+          "https://rentopedia-backend.onrender.com/api/user/me",
+          { withCredentials: true }
+        );
+        setUser(res.data);
+      } catch {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkUser();
+  }, []);
+
   return (
     <UserContext.Provider value={{ user, setUser, loading }}>
       {children}
     </UserContext.Provider>
   );
 };
-
 
 export default UserData;
 export const useUser = () => useContext(UserContext);
